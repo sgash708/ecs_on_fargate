2d52c3fcc6e04b062f466276716fe8c350c62ee7
sgash708 <sugash7081023@gmail.com>
2021-12-07 11:54:38 +0900

    共通リソースから不要なリソースを削除

--- front_site/common/main.tf
+++ front_site/common/main.tf
@@ -25,24 +25,3 @@ module "codebuild" {
   service_name  = var.service_name
   account_info  = var.account_info
 }
-
-#####################
-# 削除予定
-#####################
-# CI/CD
-module "codecommit" {
-  source = "../modules/cicd/codecommit"
-
-  service_name = var.service_name
-}
-module "codepipeline" {
-  source = "../modules/cicd/codepipeline"
-
-  cc_base_repo_name    = module.codecommit.base_repo_name
-  cc_app_repo_name     = module.codecommit.app_repo_name
-  cb_base_project_name = module.codebuild.base_project_name
-  cb_app_project_name  = module.codebuild.app_project_name
-  account_info         = var.account_info
-  s3_info              = var.s3_info
-  cd_info              = var.cd_info
-}
